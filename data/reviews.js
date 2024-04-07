388d40fc7b26296dda78245e1bb488456f8e76ac
Matthew Angelakos <93948171+MattAngelakos@users.noreply.github.com>
2024-04-06 20:59:45 -0400

    Update reviews.js
    added create for review

--- data/reviews.js
+++ data/reviews.js
@@ -17,7 +17,7 @@ const getAllReviewsForShop = async (shopId) => {
   let reviews = []
   users.forEach(user => {
     user.reviews.forEach(review => {
-      if (review.shopId === shopId) {
+      if (review.objId === shopId) {
         reviews = reviews.concat(review);
       }
   })});
@@ -41,9 +41,58 @@ const getReview = async (reviewId) => {
     return review
 }
 
+const createReview = async (userId, objId, rating, review) => {
+  userId = valid.idCheck(userId)
+  const userReview = await userData.getUser(userId)
+  const shopForReview = await shopData.getShop(objId)
+  objId = valid.idCheck(userId)
+  valid.numCheck(rating)
+  valid.intCheck(rating)
+  review = valid.stringValidate(review)
+  const currentDate = new Date();
+  const currentDateString = currentDate.toLocaleDateString('en-US', { year: 'numeric', month: '2-digit', day: '2-digit' });
+  let x = new ObjectId();
+  const newReview = {
+    _id: x,
+    objId: objId,
+    rating: rating,
+    review: review,
+    reviewDate: currentDateString
+  }
+  userReview.reviews.push(newReview)
+  shopForReview.reviews.push(x)
+  const len = shopForReview.reviews.length
+  if(len == 1){
+    shopForReview.averageRating = newReview.rating 
+  }
+  else{
+    shopForReview.averageRating = ((shopForReview.averageRating*(len-1))+newReview.rating)/len
+  }
+  const shopCollection = await shops();
+  const userCollection = await users();
+  const userUpdatedInfo = await userCollection.findOneAndUpdate(
+    {_id: new ObjectId(userId)},
+    {$set: userReview},
+    {returnDocument: 'after'}
+  )
+  if (!userUpdatedInfo) {
+    throw 'could not update product successfully';
+  }
+  const shopUpdatedInfo = await shopCollection.findOneAndUpdate(
+    {_id: new ObjectId(objId)},
+    {$set: shopForReview},
+    {returnDocument: 'after'}
+  )
+  if (!shopUpdatedInfo) {
+    throw 'could not update product successfully';
+  }
+  return newReview;
+}
+
 const exportedMethods = {
   getAllReviewsFromUser,
   getAllReviewsForShop,
+  createReview,
   getReview
 }
 export default exportedMethods;
